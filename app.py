996a7d04f0de2b7df967b14799f927ae3383516b
Andrea Jao <ndrjao@gmail.com>
2024-04-24 12:11:10 -0700

    Updated calc folder with server

--- app.py
+++ app.py
@@ -1,1 +1,51 @@
-# Put your app in here.
+from operations import add, sub, mult, div
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.get('/add')
+def add_numbers():
+    """takes in 2 numbers and return their sum"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    sum = str(add(a, b))
+
+    return sum
+
+
+@app.get('/sub')
+def subtract_numbers():
+    """takes in 2 numbers and return their difference"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    difference = str(sub(a, b))
+
+    return difference
+
+
+@app.get('/mult')
+def multiply_numbers():
+    """takes in 2 numbers and return their difference"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    product = str(mult(a, b))
+
+    return product
+
+
+@app.get('/div')
+def divide_numbers():
+    """takes in 2 numbers and return their difference"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    quotient = str(div(a, b))
+
+    return quotient
